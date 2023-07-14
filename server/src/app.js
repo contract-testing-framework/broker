459484aa505ed0ab9daccd11b1d991cbc43011b1
vu-eric <eric16259@gmail.com>
2023-07-14 15:35:27 -0700

    implement rate limiting

--- server/src/app.js
+++ server/src/app.js
@@ -9,8 +9,17 @@ import apiRouter from "./routes/api/api.js";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import loggers from "./utils/loggers.js";
+import slowDown from "express-slow-down";
 
 const app = express();
+
+const speedLimiter = slowDown({
+  windowMs: 15 * 60 * 1000,
+  delayAfter: 100,
+  delayMs: 500,
+});
+
+app.use(speedLimiter);
 
 export const srcDir = dirname(fileURLToPath(import.meta.url));
 
